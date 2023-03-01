5fa821244e40b4f41ae8fb3aa14e362737b3220d
PALOMA BREVES VESSOLECK <23111823@CFP127ALUNO.LOCAL>
2023-03-01 12:17:09 -0300

    Aula 01-03-2023 - Triângulo - Incompleto

---
+++ src/br/senai/sp/model/Triangulo.java
@@ -0,0 +1,33 @@
+package br.senai.sp.model;
+
+public class Triangulo {
+	
+	public double base;
+	public double ladoB;
+	public double ladoC;
+	public double altura;
+	public String nome;
+	
+	
+	//criando metodos da classe
+	public void mostrarDimensoes() {
+		
+		
+		System.out.println("---------------------");
+		System.out.println(nome);
+		System.out.println("---------------------");
+		System.out.println("Base: " + base);
+		System.out.println("Lado B: " + ladoB);
+		System.out.println("Lado C: " + ladoC);
+		System.out.println("Altura: " + altura);
+		calcularArea();
+		
+	}
+	
+	public void calcularArea() {
+		
+		double area = (base * altura) / 2;
+		System.out.println("Área = " + area);
+	}
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author 23111823
+ *
+ */
+module projeto_triangulo {
+}
